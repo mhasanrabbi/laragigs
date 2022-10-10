904e0351cc6cfc53d560aefae2c75bec02330c20
mhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-10-10 20:54:27 +0600

    template partials

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -1,6 +1,7 @@
 @extends('layout')
 
 @section('content')
+@include('partials._search')
 
 <h2>{{$listing['title']}}</h2>
     <p>{{$listing['description']}}</p>
--- resources/views/listings.blade.php
+++ resources/views/listings.blade.php
@@ -1,6 +1,8 @@
 @extends('layout')
 
 @section('content')
+@include('partials._hero')
+@include('partials._search')
 
 <div class="lg:grid lg:grid-cols-2 gap-4 space-y-4 md:space-y-0 mx-4">
 
@@ -16,7 +18,7 @@
         />
         <div>
             <h3 class="text-2xl">
-                <a href="show.html">{{$listing->title}}</a>
+                <a href="/listings/{{$listing->id}}">{{$listing->title}}</a>
             </h3>
             <div class="text-xl font-bold mb-4">{{$listing->company}}</div>
             <ul class="flex">
