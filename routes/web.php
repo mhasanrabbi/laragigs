bba7eb12ad5c56910108cf93f21c075d9627795d
mhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-10-11 14:11:34 +0600

    tag filter

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,15 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Listing;
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+     Common Resource Routes:
+     index - Show all listings
+     show - show single listing
+     create - show form to create new listing
+     store - store new listing
+     edit - show form to edit listing
+     update - update listing
+     destroy - delete listing
+
 */
 
 // All Listings
