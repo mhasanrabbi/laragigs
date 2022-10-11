d97e0b94927fffa3dd7a6d2dab7012e095522931
mhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-10-11 14:29:21 +0600

    search filter

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -14,5 +14,9 @@ class Listing extends Model
         if ($filters['tag'] ?? false) {
             $query->where('tags', 'like', '%' . request('tag') . '%');
         }
+        if ($filters['search'] ?? false) {
+            $query->where('title', 'like', '%' . request('search') . '%')->orWhere('description', 'like', '%' . request('search') . '%')
+                ->orWhere('location', 'like', '%' . request('search') . '%')->orWhere('tags', 'like', '%' . request('search') . '%');
+        }
     }
 }
