e5f671b7c1144e6dad320e4b35af8d604c10780d
mhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2022-10-14 20:37:05 +0600

    user login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -46,4 +46,27 @@ class UserController extends Controller
 
         return redirect('/')->with('message', 'You have been logged out!');
     }
+
+    // Show Login Form
+    public function login()
+    {
+        return view('users.login');
+    }
+
+    // Authenticate User
+    public function authenticate(Request $request)
+    {
+        $formFields = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => 'required'
+        ]);
+
+        if (auth()->attempt($formFields)) {
+            $request->session()->regenerate();
+
+            return redirect('/')->with('message', 'You are now logged in!');
+        }
+
+        return back()->withErrors(['email' => 'Invalide Credentials'])->onlyInput('email');
+    }
 }
